43548e5e880793d22373350acfc946d70e4a55da
Shruti Patil <102854147+shrutipatil23@users.noreply.github.com>
2022-06-04 19:39:26 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("\n size of int data type = %d ",sizeof(int));
+  printf("\n size of float data type = %d ",sizeof(float));
+  printf("\n size of char data type = %d",sizeof(char));
+  printf("\n size of void data type = %d",sizeof(void));
+  printf("\n size of double data type = %d",sizeof(double));
+  
+  printf("\n THANKS!!");
+  return 0;
+}
