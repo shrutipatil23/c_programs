8edea1eb69c29707dfa9d91cb5c54c31b58cb5fb
Shruti Patil <102854147+shrutipatil23@users.noreply.github.com>
2022-07-15 01:18:05 -0700

    Add files via upload

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+
+{
+    int num_1,num_2,sum;
+    printf("\n Enter any two Numbers :");
+    scanf("%d%d",&num_1,&num_2);
+
+    sum = num_1 + num_2;
+    printf("\n\a Sum of given two numbers \n %d +%d = %d",num_1,num_2,sum);
+    printf("\n\n THANKS!!!");
+    return 0;
+}
